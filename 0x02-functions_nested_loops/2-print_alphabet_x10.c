20c6f73ca1013cf7b165159e5565c5c7e9275575
Hamza_EL <ha.elassri@edu.umi.ac.ma>
2023-07-18 09:51:44 -0700

    C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
 void print_alphabet_x10(void)
 {
 int counter = 0;
