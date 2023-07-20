38e36d6574c77e4c49c029a2210fe09ace09cf38
Hamza_EL <ha.elassri@edu.umi.ac.ma>
2023-07-20 07:16:54 -0700

    C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,7 +21,7 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-			
+
 			if (digit1 == 8 && digit2 == 9)
 				continue;
 
@@ -29,7 +29,7 @@ int main(void)
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
