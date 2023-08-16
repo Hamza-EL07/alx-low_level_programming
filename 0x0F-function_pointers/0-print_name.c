2eff8393b41b1a4de3ede3bca6b98be45560aa20
Hamza_EL <ha.elassri@edu.umi.ac.ma>
2023-08-16 05:07:14 -0700

    Function to demonstrate the print_name function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,14 +4,14 @@
 /**
  * print_name function - prints a name
  * @name: name of the person
- * @(*f) apointer to a function taking a single argument of type char * and return nothing 
- *
- * Return: Nothing.
+ * @(*f) apointer to a function taking a single argument
+ * of type char * and return nothing
+ * Return:Nothing.
  */
 
-void print_name(char *name,void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	f(name);
+	return (name);
 }
