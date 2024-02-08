a0875a27e3bc514f70a7dd4f29b70057ef4f6ecd
hamza <elassri.hamza07@gmail.com>
2024-02-09 00:57:17 +0100

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit at a given index
+ * @n: number
+ * @index: index
+ * Return: 1 if it worked, -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int ulin = 1;
+
+	if (index > 63)
+		return (-1);
+
+	ulin <<= index;
+	*n = (*n | ulin);
+}
