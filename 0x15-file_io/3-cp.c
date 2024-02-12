630f72db23f34a1d7ab45b5fdea3b02d3cec1eb9
hamza <elassri.hamza07@gmail.com>
2024-02-13 00:24:41 +0100

    edited

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -9,43 +11,43 @@
 
 int main(int argc, char *argv[])
 {
-int file_from, file_to;
-int num1 = 1024, num2 = 0;
+int f_from, f_to;
+int n1 = 1024, n2 = 0;
 char buf[1024];
 
 if (argc != 3)
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
+	dprintf(STDERR_FILENO, "Usage: cp f_from f_to\n"), exit(97);
+f_from = open(argv[1], O_RDONLY);
+if (f_from == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 }
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 	| S_IRGRP | S_IWGRP | S_IROTH);
-if (file_to == -1)
+if (f_to == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	close(file_from), exit(99);
+	close(f_from), exit(99);
 }
-while (num1 == 1024)
+while (n1 == 1024)
 {
-	num1 = read(file_from, buf, 1024);
-	if (num1 == -1)
+	n1 = read(f_from, buf, 1024);
+	if (n1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	num2 = write(file_to, buf, num1);
-	if (num2 < num1)
+	n2 = write(f_to, buf, n1);
+	if (n2 < n1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
-if (close(file_from) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+if (close(f_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(100);
 
-if (close(file_to) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+if (close(f_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to), exit(100);
 
 return (0);
 }
